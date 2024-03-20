0f51f1f6ec055ff224bb7803b87b1f8b49e80879
Dhoradex <temilolaoguntamu@outlook.com>
2024-03-20 01:57:43 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FormNavbar from "./Components/FormNavbar";
+
+function App() {
+  return (
+    <div>
+      <FormNavbar />
+      <div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
