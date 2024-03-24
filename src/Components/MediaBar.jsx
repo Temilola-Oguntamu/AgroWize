0a91e1c4fbe63105aabc7c894c1c58ced719310d
sylvieshimwauwase <slvshimwa@gmail.com>
2024-03-24 23:03:24 +0200

    working on create account

---
+++ src/Components/MediaBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./MediaBar.css";
+
+/*const RegisterButton = ({ name }) => {
+    return (
+        <div className="registerButton">
+            <div className="leftLine"></div>
+            <div className="buttonText">{name}</div>
+            <div className="rightLine"></div>
+        </div>
+    );
+}*/
+const MediaBar = ({ registerText, loginText }) => {
+    return (
+        <div className="mediaBarSection">
+            <div className="registerLine">
+                <div className="leftLine"></div>
+                <div className="buttonText">{registerText}</div>
+                <div className="rightLine"></div>
+            </div>
+            <div className="mediaBar">
+                <div className="imageBar">
+                    <img src="/facebook_ic.png" alt="facebook" />
+                </div>
+                <div className="imageBar">
+                    <img src="/google_ic.png" alt="google" />
+                </div>
+                <div className="imageBar">
+                    <img src="/Vector.png" alt="vector" />
+                </div>
+            </div>
+            <div  className="termsAndConditions">
+                <p className="terms1">
+                    By Signing up, you agree to our <a className="terms2" href="/terms-of-use">Terms of Use</a> and <a className="terms3" href="/privacy-policy">Privacy Policy</a>
+                </p>
+                <p className="terms1">
+                    Already on AgroWize? <a className="terms2" href={loginText}>Log in</a>
+                </p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default MediaBar;
